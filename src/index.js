bad4ed441216dcc7883066cbccb713bcc186e5d6
songlairui <cnre@live.cn>
2018-07-10 15:19:53 +0800

    使用proxy进行遍历计算 添加悬浮提示。

--- src/index.js
+++ src/index.js
@@ -1,60 +1,47 @@
 import squarify from 'squarify'
+import { files } from './data'
+import { fileArrayToStructure } from './utils'
 
-const data = [
-  {
-    name: 'Azura',
-    value: 6,
-    color: 'red'
-  },
-  {
-    name: 'Seth',
-    value: 5,
-    color: '',
-    children: [
-      {
-        name: 'Noam',
-        value: 3,
-        color: 'orange'
-      },
-      {
-        name: 'Enos',
-        value: 2,
-        color: 'yellow'
-      }
-    ]
-  },
-  {
-    name: 'Awan',
-    value: 5,
-    color: '',
-    children: [
-      {
-        name: 'Enoch',
-        value: 5,
-        color: 'green'
-      }
-    ]
-  },
-  {
-    name: 'Abel',
-    value: 4,
-    color: 'blue'
-  },
-  {
-    name: 'Cain',
-    value: 1,
-    color: 'indigo'
-  }
-]
-const container = { x0: 0, y0: 0, x1: 100, y1: 80 }
+const data = fileArrayToStructure(files)
+const container = { x0: 0, y0: 0, x1: 100, y1: 100 }
 
 function calc() {
   const output = squarify(data, container)
   console.log('test', output)
   window.output = output
 }
+function render() {
+  const wrapper = document.querySelector('#root')
+  output.forEach((block, i) => {
+    let el = document.querySelector(`.b-${i}`)
+    if (!el) {
+      el = document.createElement('div')
+      el.classList.add('b', `b-${i}`)
+      wrapper.appendChild(el)
+    }
+    el.style.left = `${block.x0}vw`
+    el.style.top = `${block.y0}vh`
+    el.style.width = `${block.x1 - block.x0}vw`
+    el.style.height = `${block.y1 - block.y0}vh`
+  })
+}
 function init() {
   calc()
+  render()
+  const $tip = document.querySelector('#tip')
+  window.addEventListener('mousemove', e => {
+    $tip.style.left = `${e.x}px`
+    $tip.style.top = `${e.y}px`
+    const block = (
+      [].find.call(e.target.classList, a => a.indexOf('-') > -1) || ''
+    ).replace('b-', '')
+    const meta = output[+block]
+    if (meta) {
+      $tip.textContent = `${meta.name}
+${meta.full}
+`
+    }
+  })
 }
 setTimeout(() => {
   init()
---
+++ src/utils.js
@@ -0,0 +1,51 @@
+export function fileArrayToStructure(arr) {
+  const raw = {}
+  let file = arr.pop()
+  while (file) {
+    const folders = file.split('/')
+    const basename = folders.pop()
+    folders.reduce((_, folder, idx) => {
+      const fullPath = folders.slice(0, idx + 1).join('/')
+      const parentPath = folders.slice(0, idx).join('/')
+      if (!raw[fullPath]) {
+        raw[fullPath] = new Proxy(
+          {
+            name: folder,
+            type: 'folder',
+            full: fullPath,
+            children: []
+          },
+          {
+            get(target, prop, receiver) {
+              if (prop !== 'value') return Reflect.get(target, prop, receiver)
+              if (target.value !== undefined) return target.value
+              const value = target.children.reduce((a, b) => a + b.value, 0) // 求和
+              target.value = value
+              return value
+            }
+          }
+        )
+        if (parentPath) {
+          console.assert(raw[parentPath], '按执行顺序，应该已有parentPath')
+          raw[parentPath].children.push(raw[fullPath])
+        }
+      }
+    }, '')
+    let item = {
+      value: 1,
+      name: basename,
+      type: 'file',
+      full: file
+    }
+    if (folders.length) {
+      raw[folders.join('/')].children.push(item)
+    } else {
+      raw[file] = item
+    }
+    file = arr.pop()
+  }
+  console.warn(raw)
+  return Object.keys(raw)
+    .filter(key => key.indexOf('/') === -1)
+    .map(key => raw[key])
+}
